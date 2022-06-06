3f6862c78e6f0d9a96802fe76197ba050b7b0dbb
loretta-dhahabu <loreydjefwa@gmail.com>
2022-06-06 18:53:11 +0300

    Done.

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 const currentUser = "Loretta Dhahabu";
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
-const shortGreeting = `Welcome, ${currentUser[0]}!`;
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
 const firstInitial = currentUser[0];
 const restOfName = currentUser.slice(1);
